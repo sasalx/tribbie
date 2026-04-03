de2460e1ae4260bd88cf22e24fa61d408b49bfb6
sasalx <coding@despanran.com>
2026-04-03 11:44:56 +0300

    chore(config): add router & auto import logics

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: true,
+    }),
+    Components(),
+  ],
 })
